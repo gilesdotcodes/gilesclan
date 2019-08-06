96a76d93c0bbb318665576b3bc05409cb106b9e4
Stephen Giles <stephen.giles@blueskyeducation.co.uk>
2019-08-07 00:15:22 +0100

    Summary Email for staging only

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -19,7 +19,7 @@ class Notifications < ApplicationMailer
 
   def daily_summary_event_mailer(events)
     @events = events
-    emails = if ENV['SEND_TO_ALL'] == '1'
+    emails = if ENV['PROD_SUMMARY_EMAIL'] == '1'
                User.where('id IN (?)',(1..5)).pluck(:email)
              else
                ['s.giles@hotmail.co.uk']
